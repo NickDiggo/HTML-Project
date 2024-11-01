8f66e1e592d36e741140011d928a608122f0ddc2
NickDiggo <r0333915@student.thomasmore.be>
2024-11-01 18:17:21 +0100

    verder gestyled

--- Project_2/js/test.js
+++ Project_2/js/test.js
@@ -1,202 +1,194 @@
-(function () {
-  var width,
-    height,
-    largeHeader,
-    canvas,
-    ctx,
-    points,
-    target,
-    animateHeader = true;
+const menuIcon = document.querySelector("#menu-icon");
+const LinksHeader = document.querySelector(".links-header");
 
-  // Main
-  initHeader();
-  initAnimation();
-  addListeners();
+menuIcon.onclick = () => {
+  LinksHeader.classList.toggle("active");
+};
 
-  function initHeader() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    target = { x: width / 2, y: height / 2 };
+class Carrousel {
+  icons = {
+    arrowLeft: '<i class="fa-solid fa-circle-left"></i>',
+    arrowRight: '<i class="fa-solid fa-circle-right"></i>',
+    bulletActive: '<i class="fa-solid fa-circle"></i>',
+    bulletInactive: '<i class="fa-regular fa-circle"></i>',
+    fullViewMax: '<i class="fa-solid fa-maximize"></i>',
+    fullViewMin: '<i class="fa-solid fa-minimize"></i>',
+    slidePlay: '<i class="fa-solid fa-play"></i>',
+    slidePause: '<i class="fa-solid fa-pause"></i>',
+    controlsHide: '<i class="fa-solid fa-eye-slash"></i>',
+    controlsShow: '<i class="fa-solid fa-eye"></i>',
+  };
 
-    largeHeader = document.getElementById("large-header");
-    largeHeader.style.height = height + "px";
+  constructor(carrouselEl, index) {
+    this.instance = index;
+    this.currentSlideIndex = 0;
+    this.carrouselEl = carrouselEl;
+    this.carrouselEl.setAttribute("tabindex", 0);
 
-    canvas = document.getElementById("demo-canvas");
-    canvas.width = width;
-    canvas.height = height;
-    ctx = canvas.getContext("2d");
+    this.createControlEls();
+    this.initSlides();
+    this.createEventListeners();
 
-    // create points
-    points = [];
-    for (var x = 0; x < width; x = x + width / 20) {
-      for (var y = 0; y < height; y = y + height / 20) {
-        var px = x + (Math.random() * width) / 20;
-        var py = y + (Math.random() * height) / 20;
-        var p = { x: px, originX: px, y: py, originY: py };
-        points.push(p);
-      }
-    }
+    this.autoSlide = this.createAutoSlide();
+  }
 
-    // for each point find the 5 closest points
-    for (var i = 0; i < points.length; i++) {
-      var closest = [];
-      var p1 = points[i];
-      for (var j = 0; j < points.length; j++) {
-        var p2 = points[j];
-        if (!(p1 == p2)) {
-          var placed = false;
-          for (var k = 0; k < 5; k++) {
-            if (!placed) {
-              if (closest[k] == undefined) {
-                closest[k] = p2;
-                placed = true;
-              }
-            }
-          }
+  createControlEls() {
+    this.fullPageState = false;
+    this.carrouselEl.append(
+      (this.arrowLeft = this.createControlEl(
+        "control icon arrow-left",
+        this.icons.arrowLeft
+      )),
+      (this.arrowRight = this.createControlEl(
+        "control icon arrow-right",
+        this.icons.arrowRight
+      )),
+      (this.bulletsEl = this.createControlEl("bullets")),
+      // (this.fullViewBtn = this.createControlEl(
+      //   "control icon full-view-btn",
+      //   this.icons.fullViewMax
+      // )),
+      (this.playpauseBtn = this.createControlEl(
+        "control icon auto-play-btn",
+        this.icons.slidePause
+      ))
+      // (this.constrolsHideBtn = this.createControlEl(
+      //   "icon controls-hide-btn",
+      //   this.icons.controlsHide
+      // ))
+    );
+  }
+  createControlEl(className, innerHTML = "") {
+    let div = document.createElement("div");
+    div.className = className;
+    div.innerHTML = innerHTML;
+    return div;
+  }
 
-          for (var k = 0; k < 5; k++) {
-            if (!placed) {
-              if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                closest[k] = p2;
-                placed = true;
-              }
-            }
-          }
-        }
-      }
-      p1.closest = closest;
-    }
-
-    // assign a circle to each point
-    for (var i in points) {
-      var c = new Circle(
-        points[i],
-        2 + Math.random() * 2,
-        "rgba(255,255,255,0.3)"
+  initSlides() {
+    this.slides = this.carrouselEl.querySelector(".slides").children;
+    this.bullets = [];
+    for (let i = 0; i < this.slides.length; i++) {
+      //image background
+      this.slides[
+        i
+      ].style.backgroundImage = `var(--backgroundOverlay), url(${this.slides[i].src})`;
+      //bullets
+      let bulletEl = this.createControlEl(
+        "icon bullet",
+        i === this.currentSlideIndex
+          ? this.icons.bulletActive
+          : this.icons.bulletInactive
       );
-      points[i].circle = c;
+      this.bulletsEl.append(bulletEl);
+      this.bullets.push(bulletEl);
     }
   }
 
-  // Event handling
-  function addListeners() {
-    if (!("ontouchstart" in window)) {
-      window.addEventListener("mousemove", mouseMove);
+  createAutoSlide() {
+    return setInterval(() => this.slideTo(this.currentSlideIndex + 1), 5000);
+  }
+
+  createEventListeners() {
+    //mousevents
+    this.carrouselEl.addEventListener("click", () => this.carrouselEl.focus());
+    this.arrowLeft.addEventListener("click", () =>
+      this.slideTo(this.currentSlideIndex - 1, true)
+    );
+    this.arrowRight.addEventListener("click", () =>
+      this.slideTo(this.currentSlideIndex + 1, true)
+    );
+    this.fullViewBtn.addEventListener("click", () => this.toggleFullPageView());
+    this.playpauseBtn.addEventListener("click", () => this.toggleAutoSlide());
+    this.constrolsHideBtn.addEventListener("click", () =>
+      this.toggleHideControls()
+    );
+    for (let i = 0; i < this.slides.length; i++) {
+      this.slides[i].addEventListener("dblclick", () =>
+        this.toggleFullPageView()
+      );
+      this.bullets[i].addEventListener("click", () => this.slideTo(i, true));
     }
-    window.addEventListener("scroll", scrollCheck);
-    window.addEventListener("resize", resize);
-  }
 
-  function mouseMove(e) {
-    var posx = (posy = 0);
-    if (e.pageX || e.pageY) {
-      posx = e.pageX;
-      posy = e.pageY;
-    } else if (e.clientX || e.clientY) {
-      posx =
-        e.clientX +
-        document.body.scrollLeft +
-        document.documentElement.scrollLeft;
-      posy =
-        e.clientY +
-        document.body.scrollTop +
-        document.documentElement.scrollTop;
-    }
-    target.x = posx;
-    target.y = posy;
-  }
-
-  function scrollCheck() {
-    if (document.body.scrollTop > height) animateHeader = false;
-    else animateHeader = true;
-  }
-
-  function resize() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    largeHeader.style.height = height + "px";
-    canvas.width = width;
-    canvas.height = height;
-  }
-
-  // animation
-  function initAnimation() {
-    animate();
-    for (var i in points) {
-      shiftPoint(points[i]);
-    }
-  }
-
-  function animate() {
-    if (animateHeader) {
-      ctx.clearRect(0, 0, width, height);
-      for (var i in points) {
-        // detect points in range
-        if (Math.abs(getDistance(target, points[i])) < 4000) {
-          points[i].active = 0.3;
-          points[i].circle.active = 0.6;
-        } else if (Math.abs(getDistance(target, points[i])) < 20000) {
-          points[i].active = 0.1;
-          points[i].circle.active = 0.3;
-        } else if (Math.abs(getDistance(target, points[i])) < 40000) {
-          points[i].active = 0.02;
-          points[i].circle.active = 0.1;
-        } else {
-          points[i].active = 0;
-          points[i].circle.active = 0;
-        }
-
-        drawLines(points[i]);
-        points[i].circle.draw();
-      }
-    }
-    requestAnimationFrame(animate);
-  }
-
-  function shiftPoint(p) {
-    TweenLite.to(p, 1 + 1 * Math.random(), {
-      x: p.originX - 50 + Math.random() * 100,
-      y: p.originY - 50 + Math.random() * 100,
-      ease: Circ.easeInOut,
-      onComplete: function () {
-        shiftPoint(p);
-      },
+    //keyboard events
+    this.carrouselEl.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight")
+        this.slideTo(this.currentSlideIndex + 1, true);
+      else if (e.key === "ArrowLeft")
+        this.slideTo(this.currentSlideIndex - 1, true);
+      else if (e.key === "Enter") this.toggleFullPageView();
+      else if (e.key === " ") e.preventDefault() + this.toggleAutoSlide();
+      else if (Number(e.key) > 0 && Number(e.key) <= this.slides.length)
+        this.slideTo(Number(e.key) - 1, true);
+      else if (e.key.toLowerCase() === "c") this.toggleHideControls();
     });
+
+    //fullscreen change event
+    this.carrouselEl.addEventListener("fullscreenchange", () =>
+      this.toggleFullViewBtn(!window.screenTop && !window.screenY)
+    );
   }
 
-  // Canvas manipulation
-  function drawLines(p) {
-    if (!p.active) return;
-    for (var i in p.closest) {
-      ctx.beginPath();
-      ctx.moveTo(p.x, p.y);
-      ctx.lineTo(p.closest[i].x, p.closest[i].y);
-      ctx.strokeStyle = "rgba(156,217,249," + p.active + ")";
-      ctx.stroke();
+  toggleHideControls() {
+    this.carrouselEl.classList.toggle("hide-controls");
+    if (this.carrouselEl.classList.contains("hide-controls"))
+      this.constrolsHideBtn.innerHTML = this.icons.controlsHide;
+    else this.constrolsHideBtn.innerHTML = this.icons.controlsShow;
+  }
+
+  slideTo(slideIndex, userAction = false) {
+    let newIndex = (slideIndex + this.slides.length) % this.slides.length;
+    this.bullets[this.currentSlideIndex].innerHTML = this.icons.bulletInactive;
+    this.currentSlideIndex = newIndex;
+    this.slides[0].style.marginLeft = -100 * this.currentSlideIndex + "%";
+    this.bullets[this.currentSlideIndex].innerHTML = this.icons.bulletActive;
+    if (userAction) this.toggleAutoSlide(true);
+  }
+
+  toggleAutoSlide(reset = false) {
+    if (reset) {
+      //reset
+      clearInterval(this.autoSlide);
+      this.autoSlide = this.createAutoSlide();
+    } else if (this.autoSlide) {
+      //stop
+      clearInterval(this.autoSlide);
+      this.autoSlide = false;
+      this.playpauseBtn.innerHTML = this.icons.slidePlay;
+    } else {
+      //start
+      this.autoSlide = this.createAutoSlide();
+      this.playpauseBtn.innerHTML = this.icons.slidePause;
     }
   }
 
-  function Circle(pos, rad, color) {
-    var _this = this;
+  // toggleFullPageView() {
+  //   if (!this.fullPageState) {
+  //     if (this.carrouselEl.requestFullscreen)
+  //       this.carrouselEl.requestFullscreen();
+  //     else if (this.carrouselEl.webkitRequestFullscreen)
+  //       this.carrouselEl.webkitRequestFullscreen(); /* Safari */
+  //     else if (this.carrouselEl.msRequestFullscreen)
+  //       this.carrouselEl.msRequestFullscreen(); /* IE11 */
+  //   } else {
+  //     if (document.exitFullscreen) document.exitFullscreen();
+  //     else if (document.webkitExitFullscreen)
+  //       document.webkitExitFullscreen(); /* Safari */
+  //     else if (document.msExitFullscreen)
+  //       document.msExitFullscreen(); /* IE11 */
+  //   }
+  // }
 
-    // constructor
-    (function () {
-      _this.pos = pos || null;
-      _this.radius = rad || null;
-      _this.color = color || null;
-    })();
+  // toggleFullViewBtn(fullscreen) {
+  //   this.carrouselEl.classList.toggle("full-view");
+  //   this.fullPageState = !this.fullPageState;
+  //   if (fullscreen) this.fullViewBtn.innerHTML = this.icons.fullViewMin;
+  //   else this.fullViewBtn.innerHTML = this.icons.fullViewMax;
+  // }
+}
 
-    this.draw = function () {
-      if (!_this.active) return;
-      ctx.beginPath();
-      ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-      ctx.fillStyle = "rgba(156,217,249," + _this.active + ")";
-      ctx.fill();
-    };
-  }
-
-  // Util
-  function getDistance(p1, p2) {
-    return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-  }
-})();
+const carrouselEls = document.querySelectorAll(".carrousel");
+const carrousels = [];
+carrouselEls.forEach((carrouselEl, i) => {
+  carrousels.push(new Carrousel(carrouselEl, i));
+});
